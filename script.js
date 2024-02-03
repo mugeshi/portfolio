b256ae9815344cdefe849afa9b39fe956de73c58
mugeshi <nicolemugeshi@gmail.com>
2024-02-03 11:57:31 +0300

    updated scriptfile

---
+++ script.js
@@ -0,0 +1,20 @@
+// Select all elements with the class "tab-links" and "tab-contents"
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+// Function to switch between tabs
+function opentab(tabname) {
+    // Remove "active-link" class from all tab links
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    // Remove "active-tab" class from all tab contents
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    // Add "active-link" class to the clicked tab link
+    event.currentTarget.classList.add("active-link");
+
+    // Add "active-tab" class to the corresponding tab content
+    document.getElementById(tabname).classList.add("active-tab");
+}
