d3a15e01a1962e29251929165adeb3aaaa0f45e2
mugeshi <nicolemugeshi@gmail.com>
2024-02-20 22:56:29 +0300

    updated contactemail

--- contact form to email.js
+++ contact form to email.js
@@ -1,19 +1,44 @@
-function emailSend(){
-
-    var userName = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var message = document.getElementById('message').value;
-
-
-    Email.send({
-    Host : "smtp.elasticemail.com",
-    Username : "millycyrus@gmail.com",
-    Password : "AF1C740023C1A31BF0604839F91064B31668",
-    To : "nicolemugeshi@gmail.com",
-    From : "millycyrus@gmail.com",
-    Subject : "This is the subject",
-    Body : message
-}).then(
-  message => alert(message)
+let cf = document.getElementById("contactForm");
+cf.addEventListener(
+  "submit",
+  (e) => {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    handleSubmit();
+  },
+  false
 );
+
+let submitbtn = document.getElementById("submitbtn");
+submitbtn.addEventListener(
+  "submit",
+  (e) => {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+  },
+  false
+);
+
+function handleSubmit() {
+  emailSend();
+  // reset();
+  // return false,
+
+  console.log("formcontact");
+}
+
+function emailSend() {
+  var userName = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "stellamugy@gmail.com",
+    Password: "133AD6EF5DAB307314350300B1B6B6712B0F",
+    To: "nicolemugeshi@gmail.com",
+    From: "nicolemugeshi@gmail.com",
+    Subject: "Message from portfolio: " + userName,
+    Body: message + " from " + email,
+  }).then((message) => alert(message));
 }
